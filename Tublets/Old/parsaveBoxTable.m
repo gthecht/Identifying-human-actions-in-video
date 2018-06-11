c7a10e068ef3779bfb44b5c03876d53cf3a800d7
gthecht <gdpstd@gmail.com>
2018-06-11 20:20:27 +0300

    deleted 'tublets\old' directory

--- Tublets/Old/parsaveBoxTable.m
+++
@@ -1,7 +0,0 @@
-function [] = parsaveBoxTable(tablesdir, boxTable,boxTableName)
-% saves boxTable from within the parfor.
-% save table to tablesdir:
-% first the name:
-save([tablesdir,'\',boxTableName], 'boxTable');
-end
-
